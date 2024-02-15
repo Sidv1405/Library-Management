f08e12d81539bfff66f22944bb00903a584179fd
Dang Van Si <dangvansi1405@gmail.com>
2024-02-15 15:50:02 +0700

    init welcome screen

---
+++ app/src/main/java/com/sidv/quanlythuvien/View/WelcomeScreenActivity.java
@@ -0,0 +1,18 @@
+package com.sidv.quanlythuvien.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.sidv.quanlythuvien.R;
+
+public class WelcomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+
+    }
+}
