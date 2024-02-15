5f99f62316738fca11be7564d2727cd3f286238a
Dang Van Si <dangvansi1405@gmail.com>
2024-02-15 19:33:54 +0700

    add Login screen

---
+++ app/src/main/java/com/sidv/quanlythuvien/View/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.sidv.quanlythuvien.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sidv.quanlythuvien.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/sidv/quanlythuvien/View/WelcomeActivity.java
+++ app/src/main/java/com/sidv/quanlythuvien/View/WelcomeActivity.java
@@ -2,17 +2,23 @@ package com.sidv.quanlythuvien.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
 import com.sidv.quanlythuvien.R;
 
-public class WelcomeScreenActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        }, 3000);
     }
 }
