a284b37431b69614f419174b7c07236897211017
Dang Van Si <dangvansi1405@gmail.com>
2024-02-16 14:40:37 +0700

    fix DTO PhieuMuon.java

--- app/src/main/java/com/sidv/quanlythuvien/DTO/PhieuMuon.java
+++ app/src/main/java/com/sidv/quanlythuvien/DTO/PhieuMuon.java
@@ -3,93 +3,93 @@ package com.sidv.quanlythuvien.DTO;
 import java.util.Date;
 
 public class PhieuMuon {
-    private int MAPM;
-    private String MATT;
-    private int MATV;
-    private int MASACH;
-    private int TIENTHUE;
-    private int TRASACH;
-    private Date NGAY;
+    private int maPhieuMuon;
+    private String maThuThu;
+    private int maThanhVien;
+    private int maSach;
+    private int tienThue;
+    private int traSach;
+    private Date ngay;
 
     public PhieuMuon() {
-    }
-
-    public PhieuMuon(int MAPM, String MATT, int MATV, int MASACH, int TIENTHUE, int TRASACH, Date NGAY) {
-        this.MAPM = MAPM;
-        this.MATT = MATT;
-        this.MATV = MATV;
-        this.MASACH = MASACH;
-        this.TIENTHUE = TIENTHUE;
-        this.TRASACH = TRASACH;
-        this.NGAY = NGAY;
-    }
-
-    public int getMAPM() {
-        return MAPM;
-    }
-
-    public void setMAPM(int MAPM) {
-        this.MAPM = MAPM;
-    }
-
-    public String getMATT() {
-        return MATT;
-    }
-
-    public void setMATT(String MATT) {
-        this.MATT = MATT;
-    }
-
-    public int getMATV() {
-        return MATV;
-    }
-
-    public void setMATV(int MATV) {
-        this.MATV = MATV;
-    }
-
-    public int getMASACH() {
-        return MASACH;
-    }
-
-    public void setMASACH(int MASACH) {
-        this.MASACH = MASACH;
-    }
-
-    public int getTIENTHUE() {
-        return TIENTHUE;
-    }
-
-    public void setTIENTHUE(int TIENTHUE) {
-        this.TIENTHUE = TIENTHUE;
-    }
-
-    public int getTRASACH() {
-        return TRASACH;
-    }
-
-    public void setTRASACH(int TRASACH) {
-        this.TRASACH = TRASACH;
-    }
-
-    public Date getNGAY() {
-        return NGAY;
-    }
-
-    public void setNGAY(Date NGAY) {
-        this.NGAY = NGAY;
     }
 
     @Override
     public String toString() {
         return "PhieuMuon{" +
-                "MAPM=" + MAPM +
-                ", MATT='" + MATT + '\'' +
-                ", MATV=" + MATV +
-                ", MASACH=" + MASACH +
-                ", TIENTHUE=" + TIENTHUE +
-                ", TRASACH=" + TRASACH +
-                ", NGAY=" + NGAY +
+                "maPhieuMuon=" + maPhieuMuon +
+                ", maThuThu='" + maThuThu + '\'' +
+                ", maThanhVien=" + maThanhVien +
+                ", maSach=" + maSach +
+                ", tienThue=" + tienThue +
+                ", traSach=" + traSach +
+                ", ngay=" + ngay +
                 '}';
+    }
+
+    public int getMaPhieuMuon() {
+        return maPhieuMuon;
+    }
+
+    public void setMaPhieuMuon(int maPhieuMuon) {
+        this.maPhieuMuon = maPhieuMuon;
+    }
+
+    public String getMaThuThu() {
+        return maThuThu;
+    }
+
+    public void setMaThuThu(String maThuThu) {
+        this.maThuThu = maThuThu;
+    }
+
+    public int getMaThanhVien() {
+        return maThanhVien;
+    }
+
+    public void setMaThanhVien(int maThanhVien) {
+        this.maThanhVien = maThanhVien;
+    }
+
+    public int getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(int maSach) {
+        this.maSach = maSach;
+    }
+
+    public int getTienThue() {
+        return tienThue;
+    }
+
+    public void setTienThue(int tienThue) {
+        this.tienThue = tienThue;
+    }
+
+    public int getTraSach() {
+        return traSach;
+    }
+
+    public void setTraSach(int traSach) {
+        this.traSach = traSach;
+    }
+
+    public Date getNgay() {
+        return ngay;
+    }
+
+    public void setNgay(Date ngay) {
+        this.ngay = ngay;
+    }
+
+    public PhieuMuon(int maPhieuMuon, String maThuThu, int maThanhVien, int maSach, int tienThue, int traSach, Date ngay) {
+        this.maPhieuMuon = maPhieuMuon;
+        this.maThuThu = maThuThu;
+        this.maThanhVien = maThanhVien;
+        this.maSach = maSach;
+        this.tienThue = tienThue;
+        this.traSach = traSach;
+        this.ngay = ngay;
     }
 }
